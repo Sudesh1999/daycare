d1380e089b99142b19a017b209b9025eb43caa67
Sudesh1999 <67303209+Sudesh1999@users.noreply.github.com>
2020-10-25 04:13:16 -0400

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+
+jQuery(document).ready(function(){
+
+// Equal height
+	if (jQuery(".mHeight").length > 0) {
+		jQuery(".mHeight").matchHeight();
+	}	
+// Equal height
+	if (jQuery(".mealHeight").length > 0) {
+		jQuery(".mealHeight").matchHeight();
+	}
+// Equal height
+	if (jQuery(".picsHeight").length > 0) {
+		jQuery(".picsHeight").matchHeight();
+	}
+				
+});
+
+
